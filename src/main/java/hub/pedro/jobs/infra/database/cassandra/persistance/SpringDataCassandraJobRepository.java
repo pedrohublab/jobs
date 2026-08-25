7cc645641895cabe178d05fe72d953bcc790ca23
Pedro Barbosa <pedro.barbosa.dev@gmail.com>
2026-08-25 01:31:11 -0300

    Cassandra abstraction

---
+++ src/main/java/hub/pedro/jobs/infra/database/cassandra/persistance/SpringDataCassandraJobRepository.java
@@ -0,0 +1,7 @@
+package hub.pedro.jobs.infra.database.cassandra.persistance;
+
+import java.util.UUID;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+
+public interface SpringDataCassandraJobRepository
+    extends CassandraRepository<JobCassandraModel, UUID> {}
